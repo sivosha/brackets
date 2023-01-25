dbf39f8f1ca4a6ebd08c36397adf699edff7bea3
sivosha <sivosha1@gmail.com>
2023-01-25 08:36:35 -0700

    init: solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let stck = [];
+  for(let i=0; i<str.length; i++) {
+    let chr = stck[stck.length-1];
+    if (getCloseBracket(chr, bracketsConfig) == str[i]) {
+      stck.pop();
+    } else if(getCloseBracket(str[i], bracketsConfig) != '') {
+      stck.push(str[i]);
+    } else
+      return false;
+  }
+
+  return stck.length == 0;
+}
+
+function getCloseBracket(chr, bracketsConfig) {
+  for(let i=0; i<bracketsConfig.length; i++) {
+    if(bracketsConfig[i][0] == chr) return bracketsConfig[i][1];
+  }
+  return '';
 }
